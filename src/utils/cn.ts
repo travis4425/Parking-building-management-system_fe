3fadeaca038c0e8d10531f432922c37795150a54
travis4425 <sockhanh4425@gmail.com>
2026-05-26 18:33:43 +0700

    thêm hàm tiện ích cn để kết hợp class

---
+++ src/utils/cn.ts
@@ -0,0 +1,7 @@
+// Utility kết hợp class names với tailwind-merge (tránh conflict class)
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
